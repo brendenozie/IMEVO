fe58d48ed33c60057022be3f7347fd60c864fcef
brendenozie <brendenozie@gmail.com>
2026-02-13 11:21:49 +0300

    Refactor Hero component image styling for improved visibility on mobile

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,9 +17,12 @@ export function Hero() {
         <div className="absolute inset-0 bg-black/50 z-10" />
         <div className="absolute inset-0 bg-gradient-to-r from-black/60 via-black/5 to-transparent z-10" />
         <div className="absolute inset-0 bg-gradient-to-t from-black/60 via-transparent to-transparent z-10" />
+        {/* on mobile lets make the image especially its right side more visible */}
         <Image 
           src="/close-up-delivery-person-with-parcel.png" 
-          className="w-full h-full object-cover scale-105 animate-slow-zoom" 
+          className="object-cover object-center w-full h-full animate-slow-zoom"
+          
+          // className="w-full h-full object-cover scale-105 animate-slow-zoom" 
           alt="Rail Freight and Logistics"
           fill
           priority
